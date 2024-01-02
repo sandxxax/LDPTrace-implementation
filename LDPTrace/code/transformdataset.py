72d23953daf844217759c63d251a3fa0fafb1292
Sandhya V <122590982+sandxxax@users.noreply.github.com>
2024-01-03 02:26:05 +0800

    Add files via upload

---
+++ LDPTrace/code/transformdataset.py
@@ -0,0 +1,30 @@
+def transform_dataset(original_dataset):
+    transformed_dataset = []
+    trajectory_id = -1  # Initialize the trajectory ID to -1
+
+    for line in original_dataset:
+        if line.startswith('newpoint'):
+            trajectory_id += 1  # Increment the trajectory ID for new points
+            transformed_dataset.append(f"#{trajectory_id}:")
+            transformed_dataset.append(f">0: {line.split()[5]},{line.split()[6]}")
+        elif line.startswith('point'):
+            transformed_dataset[-1] += f"; {line.split()[5]},{line.split()[6]}"
+        else:
+            # Handling additional cases if any specific action is required
+            pass
+
+    return transformed_dataset
+
+
+if __name__ == "__main__":
+    # Read the original dataset
+    with open('C:/Users/iamar/Desktop/Internship/LDPTrace/LDPTrace/data/original_oldenburg.dat', 'r') as file:
+        original_dataset = file.readlines()
+
+    # Transform the dataset
+    transformed_dataset = transform_dataset(original_dataset)
+
+    # Save the transformed dataset
+    with open('C:/Users/iamar/Desktop/Internship/LDPTrace/LDPTrace/data/oldenburg.dat', 'w') as file:
+        file.write('\n'.join(transformed_dataset))
+    print("done")
